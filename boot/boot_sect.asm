4621f160f168b3528048bec9ab6eefb19432bd05
Red-mar <redmarvanderiet@gmail.com>
2019-12-26 16:14:52 +0100

    input loop

--- boot/boot_sect.asm
+++ boot/boot_sect.asm
@@ -6,7 +6,8 @@ KERNEL_OFFSET equ 0x1000
     mov sp, bp
 
 
-    call print_keyboard
+    call set_cursor_shape
+    call main_loop
     jmp $
 
 ;    call load_kernel
@@ -21,25 +22,25 @@ KERNEL_OFFSET equ 0x1000
 %include "prints.asm"
 %include "disk_load.asm"
 %include "switch_pm.asm"
-%include "keyboard.asm"
+%include "input.asm"
 
 [bits 16]
-cursor:
+main_loop:
+    call sleep_bios
+    call input
+    call set_cursor_pos
+    jmp main_loop
+
+set_cursor_shape:
     mov cx, 0x0007              ; fill line 0 - 7
     mov ah, 0x01                ; set cursor shape
     int 0x10
-    call set_cursor
     ret
 
-set_cursor:
-    mov dx, 0x0f00              ; dh = row, dl = column
+set_cursor_pos:                     ; dh = row, dl = column
     mov bh, 0x0                 ; page number[?]
-cur_loop:
-    inc dl
-    mov ah, 0x02                ; set cursor pos
+    mov ah, 0x02
     int 0x10
-    call sleep_bios
-    jmp cur_loop
     ret
 
 ticks dd 0
@@ -70,10 +71,10 @@ BEGIN_PM:
     call KERNEL_OFFSET
     jmp $
 
-MSG_REAL_MODE db "16-bit mode", 0
+;MSG_REAL_MODE db "16-bit mode", 0
 ;MSG_PROT_MODE db "32-bit protected mode", 0
 ;MSG_LOAD_KERNEL db "Loading KERNEL", 0
 ;MSG_LOADED db "Loaded KERNEL", 0
 
-times 510-($-$$) db 0
-dw 0xaa55
+times 510-($-$$) db 0           ;fill empty space
+dw 0xaa55                       ;magic number
