0f7a58159cc471e196d70048a7f858abf92b8149
redmar1 <redmar@d-centralize.nl>
2019-12-26 22:55:51 +0100

    fix: print_hex bug

--- boot/prints.asm
+++ boot/prints.asm
@@ -1,6 +1,7 @@
 print_hex:
     pusha
 
+    xor cl, cl
 iter_hex:
     shr dx, 0x1                 ; shift the adress
     jc bzero                    ; if cf is zero go to z
@@ -56,7 +57,12 @@ print_loop:
     mov al, [bx]                ; get the char
     cmp al, 0                   ; if 0 end
     je print_loop_end
+    push bx
+;    mov bh, al
+;    and bx, 0x000e              ; color depend on pointer hack :)
+    mov bx, 0x0005
     int 0x10                    ; else print it
+    pop bx
     add bx, 0x0001              ; advance the pointer
     jmp print_loop              ; and start again
 print_loop_end:
@@ -68,21 +74,21 @@ full_hex_i:   db 6
 HEX_OUT: db '0x0000', 0
 hex_chars: db '0123456789ABCDEF'
 
-[bits 32]
-VIDEO_MEMORY equ 0xb8000
-WHITE_ON_BLACK equ 0x0f
-print_pm:
-    pusha
-    mov edx, VIDEO_MEMORY
-print_pm_loop:
-    mov al, [ebx]                ; get the char
-    mov ah, WHITE_ON_BLACK
-    cmp al, 0                   ; if 0 end
-    je print_pm_loop_end
-    mov [edx], ax
-    add ebx, 1              ; advance the pointer
-    add edx, 2
-    jmp print_pm_loop              ; and start again
-print_pm_loop_end:
-    popa
-    ret
+;[bits 32]
+;VIDEO_MEMORY equ 0xb8000
+;WHITE_ON_BLACK equ 0x0f
+;print_pm:
+;    pusha
+;    mov edx, VIDEO_MEMORY
+;print_pm_loop:
+;    mov al, [ebx]                ; get the char
+;    mov ah, WHITE_ON_BLACK
+;    cmp al, 0                   ; if 0 end
+;    je print_pm_loop_end
+;    mov [edx], ax
+;    add ebx, 1              ; advance the pointer
+;    add edx, 2
+;    jmp print_pm_loop              ; and start again
+;print_pm_loop_end:
+;    popa
+;    ret
